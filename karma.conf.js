fbf16845ec8479299b4a76200f1437d8e6c1718c
rafikidota <lesmes240698@gmail.com>
2022-06-07 01:38:54 -0400

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/country-app'),
+      dir: require('path').join(__dirname, './coverage/rafiki-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RafikiModule } from './rafiki/rafiki.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RafikiModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
